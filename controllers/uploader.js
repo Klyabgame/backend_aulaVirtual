f1967542a382b03c19d1a14827f4e126c4f973e8
franz schwartz <gavino_10@hotmail.es>
2023-11-12 21:47:22 -0500

    avanzando uploader2

--- controllers/uploader.js
+++ controllers/uploader.js
@@ -1,23 +1,51 @@
 const { response, request } = require("express")
 const { v4: uuidv4 } = require('uuid');
+const cloudinary = require('cloudinary').v2
+
+const Usuario=require('../models/usuario');
 
 
 const extensionesValidas=['jpg','png,PNG,GIF,TIFF,PSD,RAW'];
 
+const coleccionesPermitidas=[
+    'usuarios'
+]
+
 const uploaderImage =async(req=request,res=response)=>{
 
     const {coleccion,id}=req.params;
+    let model;
+    switch (coleccion) {
+        case 'usuarios':
+             model=await Usuario.findById(id);
+            if (!model) {
+                return res.status(400).json({
+                    msg:'el usuario ingresado no se encontro en la bd'
+                })
+            }
+            break;
+    
+        default:
+            return res.status(400).json({
+                msg:'no se encontraron en las colecciones'
+            })
+    }
+
     const {name,tempFilePath}=req.files.archivo
+
     const nameArray=name.split('.');
     const nameImg=uuidv4();
     const nameType=nameArray[nameArray.length-1];
     const urlImg=nameImg+'.'+nameType;
-    console.log(urlImg);
+
+    //const algo=archivo.join(__dirname,'../uploader',urlImg);
+    const {secure_url}=await cloudinary.uploader.upload(tempFilePath);
+
+    model.img=secure_url;
+    model.save();
 
     res.json({
-        msg:'subiendoimg',
-        coleccion,
-        id
+        mod:model
     })
 
 
